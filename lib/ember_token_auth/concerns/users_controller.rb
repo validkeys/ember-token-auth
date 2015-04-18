9a888a5003ef651fce8a9085ff03853197b41135
Kyle Davis <validkeys@gmail.com>
2015-04-18 19:32:31 -0400

    Fixed error in user signups

--- lib/ember_token_auth/concerns/users_controller.rb
+++ lib/ember_token_auth/concerns/users_controller.rb
@@ -8,15 +8,16 @@ module EmberTokenAuth
 
 
     def create
-      unless respond_to?(:user_params)
+      unless defined? self.user_params
         return render json: {err: "Please create a user_params property on your users controller"}, status: 500
       end
       @user = User.authenticate(params[:user][:email], params[:user][:password])
+
       if @user.nil?
         @user = User.new(user_params)
         # @user.password = params[:user][:password]
         unless @user.save
-          return render json: {errors: @user.errors.full_messages}, status: 422
+          return render json: {errors: @user.errors}, status: 422
         end
       end
       
